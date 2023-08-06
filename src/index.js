0ea77cecb0299a39cc39dddbe20c9670ff09923b
Bilge <134232792+goksubilge@users.noreply.github.com>
2023-08-06 10:14:34 +0300

    add index loggers & devtools

--- src/index.js
+++ src/index.js
@@ -4,21 +4,33 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
-import { ToastContainer, toast } from "react-toastify";
+// import { ToastContainer, toast } from "react-toastify";
 // import "react-toastify/dist/React-Toastify.css";
+
+//redux
 import { Provider } from "react-redux";
 import { legacy_createStore as createStore } from "redux";
 import reducer, { baslangicDegerleri } from "./reducers";
 
+//logger
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { composeWithDevTools } from "redux-devtools-extension";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const migros = createStore(reducer, baslangicDegerleri);
+// ini redux store
+const migros = createStore(
+  reducer,
+  baslangicDegerleri,
+  composeWithDevTools(applyMiddleware(logger))
+);
 
 root.render(
   <Provider store={migros}>
     <BrowserRouter>
       <>
-        <ToastContainer />
+        {/* <ToastContainer /> */}
         <App />
       </>
     </BrowserRouter>
